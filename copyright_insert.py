cc92d7a5ec798ad67eb13fe320c7e2a01b6cad44
VinnyBabuManjaly <vinnybabumanjaly@gmail.com>
2020-02-14 10:03:34 -0800

    changes for empty values in ignorefile path

--- copyright_insert.py
+++ copyright_insert.py
@@ -64,6 +64,13 @@ class InsertCopyRight:
                                 if _ignore_path[-1] == '/' and _ignore_path.rpartition('/')[0] in _root:
                                     print('check2: ', _files)
 
+                            if _path == '.' and _ignore_path[-1] == '/':
+                                if './'+_ignore_path == _root+'/':
+                                    print(
+                                        'yahoo : ', _ignore_path,
+                                        _root, _files,
+                                    )
+
                             if _ignore_path not in _root:
                                 _value = False
                             else:
@@ -96,7 +103,7 @@ class InsertCopyRight:
                     print(
                         _file, 'is an empty file, hence not adding the copyright or license to the file',
                     )
-                if content:
+                else:
                     if self.copyright_check(content):
                         print('License string already exists in ', _file)
                     else:
